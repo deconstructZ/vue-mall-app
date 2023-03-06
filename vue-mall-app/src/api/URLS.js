ebcb30d3c525ffd06f988c971f0192eb091060e1
wz <gglakvw@163.com>
2023-03-06 15:04:32 +0800

    update 1

--- vue-mall-app/src/api/URLS.js
+++
@@ -1,9 +0,0 @@
-const url = "https://mallapi.duyiedu.com/goods/"
-export const urls = {
-  getsidebar: "/getsidebar",
-  getGoodsList: "/getGoodsList",
-  likeSearch: "/likeSearch",
-  search: "/search",
-  getGoodsByIds: "/getGoodsByIds"
-}
-export default url
